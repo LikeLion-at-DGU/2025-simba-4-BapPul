e33aa1bd53ba0fda7e931d8269bebf97cc0ce09b
onowoey <cg10046@naver.com>
2025-06-21 04:51:21 +0900

    리뷰 리스트 보기

--- project/menu/views.py
+++ project/menu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Store, Menu
+from .models import Store, Menu , Review
 
 def store_detail(request, store_id, menu_id):
     store = get_object_or_404(Store, id=store_id)
@@ -10,4 +10,15 @@ def store_detail(request, store_id, menu_id):
         'store': store,
         'menus': menus,
         'selected_menu': selected_menu,  # 강조 표시용
+    })
+
+def store_review_list(request, store_id):
+    store = get_object_or_404(Store, id=store_id)
+
+    # 이 음식점의 모든 리뷰 (연결된 메뉴들에 달린 리뷰들 포함)
+    reviews = Review.objects.filter(menu__store=store).select_related('menu', 'writer')
+
+    return render(request, 'menu/store_review_list.html', {
+        'store': store,
+        'reviews': reviews,
     })
