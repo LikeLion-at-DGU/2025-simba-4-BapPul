089f167b0bb16eaa9f451ddaafb2cfd394c40faa
junh5ng <wnsghd1130@dgu.ac.kr>
2025-06-20 19:41:55 +0900

    login/signup feature 구현 완료

--- project/project/views.py
+++ project/project/views.py
@@ -1,9 +1,9 @@
 # project/views.py
 
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 def root_redirect_view(request):
     if request.user.is_authenticated:
         return redirect('search:home')  # 로그인한 경우
     else:
-        return redirect('accounts:login')  # 로그인 안 한 경우
+        return redirect('accounts:start')  # 로그인 안 한 경우
