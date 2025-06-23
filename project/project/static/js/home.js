140c5a93e210e0557eb3f61fba74a79c37282cc4
Hello-Worldismine <alstnqls02@naver.com>
2025-06-24 02:18:34 +0900

    static 폴더 수정

---
+++ project/project/static/js/home.js
@@ -0,0 +1,12 @@
+document.getElementById("price-form").addEventListener("submit", function (e) {
+    const priceInput = document.getElementById("price-input");
+    const warning = document.getElementById("price-warning");
+    const priceValue = Number(priceInput.value);
+
+    if (!priceValue || priceValue < 1000) {
+      e.preventDefault(); // 폼 제출 막기
+      warning.style.display = "block"; // 경고 표시
+    } else {
+      warning.style.display = "none"; // 통과 시 경고 숨기기
+    }
+  });
