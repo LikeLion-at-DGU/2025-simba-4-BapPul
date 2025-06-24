781e7fc32a5fae1a5fc3125d00b6593dc45df12a
Hello-Worldismine <alstnqls02@naver.com>
2025-06-25 00:39:21 +0900

    마이페이지 수정

--- project/project/urls.py
+++ project/project/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('menu/', include('menu.urls')),
     path('search/', include('search.urls')),
     path('stamp/',include('stamp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
